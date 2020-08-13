8ade0f4c33837f12803869e56a2afcfbe384d820
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-08-13 08:11:30 +0200

    Navigation and Routing Basics

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 import { ProductService } from './product.service';
 @Component({
-  selector: 'pm-products',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product‑list.component.css'],
 })
